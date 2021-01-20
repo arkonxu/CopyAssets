0ed2bbc2b81d6d8be577e97d37a0c172cabea504
Carlos Soler <>
2021-01-20 13:16:02 +0100

    Fixed

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -2,15 +2,15 @@ package com.example.demo.view
 
 import javafx.scene.control.Alert
 import javafx.scene.control.TextField
+import javafx.scene.image.Image
 import org.apache.commons.io.FileUtils
 import tornadofx.*
 import java.io.File
-import java.nio.file.CopyOption
-import java.nio.file.Files
+import java.net.URL
 import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
 import java.util.*
 import java.util.stream.Collectors
+import javax.swing.ImageIcon
 import kotlin.streams.toList
 
 class MainView : View("DirectoryChooser") {
@@ -23,8 +23,14 @@ class MainView : View("DirectoryChooser") {
     private var brandings: List<File>? = null
     private var assets: List<File>? = null
     private var copyAssets: MutableList<File> = mutableListOf()
+    private val logo: Image = Image("file:///Users/carlossoler/IdeaProjects/CopyAssets/src/main/resources/odiloLogo.png")
+    var iconURL: URL = URL("file:///Users/carlossoler/IdeaProjects/CopyAssets/src/main/resources/odiloLogo.png")
+    var imageIcon: java.awt.Image = ImageIcon(iconURL).image
+    private val dockLogo: java.awt.Image = imageIcon
 
     init {
+        com.apple.eawt.Application.getApplication().setDockIconImage(dockLogo)
+        addStageIcon(logo)
         with(root) {
             title = "CopyAssets"
             center = form {
@@ -59,36 +65,37 @@ class MainView : View("DirectoryChooser") {
                         brandings = Arrays.stream(brandingRoot?.listFiles()).filter { it.name != ".DS_Store" }.toList();
                         assets = Arrays.stream(assetsToCopy?.listFiles()).collect(Collectors.toList());
                         brandings?.forEach { branding ->
-                            val brandingAssets =
-                                branding.listFiles()?.filter { it.name == "Assets.xcassets" }?.get(0)?.listFiles()
-                                    ?.toList()
-                            assets?.forEach { assetToCompare ->
-                                if(!brandingAssets?.map { it.name }?.contains(assetToCompare.name)!!) {
-                                    copyAssets.add(assetToCompare)
+                            if(branding.path != assetsToCopy?.parent) {
+                                val brandingAssets =
+                                    branding.listFiles()?.filter { it.name == "Assets.xcassets" }?.get(0)?.listFiles()
+                                        ?.toList()
+                                assets?.forEach { assetToCompare ->
+                                    if (!brandingAssets?.map { it.name }?.contains(assetToCompare.name)!!) {
+                                        copyAssets.add(assetToCompare)
+                                    }
                                 }
-                            }
-                            if(!copyAssets.isEmpty()) {
-                                copyAssets.forEach {
-                                    Files.copy(
-                                        it.toPath(),
-                                        Paths.get(branding.listFiles()?.filter { it.name == "Assets.xcassets" }?.get(0)
-                                            ?.toString() + "/" + it.name
-                                        ),
-                                        StandardCopyOption.REPLACE_EXISTING
-                                    )
-                                    FileUtils.copyDirectory(
-                                        it,
-                                        Paths.get(branding.listFiles()?.filter { it.name == "Assets.xcassets" }?.get(0)
-                                            ?.toString() + "/" + it.name
-                                        ).toFile()
-                                    )
-                                    println(
-                                        "Copying " + it.name + " to " + branding.listFiles()
-                                            ?.filter { it.name == "Assets.xcassets" }?.get(0)!!
+                                if (!copyAssets.isEmpty()) {
+                                    copyAssets.forEach { asset ->
+                                        FileUtils.copyDirectory(
+                                            asset,
+                                            Paths.get(
+                                                branding.listFiles()?.filter { it.name == "Assets.xcassets" }?.get(0)
+                                                    ?.toString() + "/" + asset.name
+                                            ).toFile()
+                                        )
+                                        println(
+                                            "Copying " + asset.name + " to " + branding.listFiles()
+                                                ?.filter { it.name == "Assets.xcassets" }?.get(0)!!
+                                        )
+                                    }
+                                } else {
+                                    alert(
+                                        Alert.AlertType.ERROR,
+                                        "Error",
+                                        "No hay ningun archivo para copiar",
+                                        owner = currentWindow
                                     )
                                 }
-                            } else {
-                                alert(Alert.AlertType.ERROR, "Error", "No hay ningun archivo para copiar", owner = currentWindow)
                             }
                         }
                     }
