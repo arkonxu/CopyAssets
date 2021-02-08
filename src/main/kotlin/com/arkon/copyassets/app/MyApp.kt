98405995b23ff8a865a37f216fa75e9f18e81d86
Carlos Soler <>
2021-02-08 11:32:00 +0100

    Improvements and refactor

--- src/main/kotlin/com/arkon/copyassets/app/MyApp.kt
+++ src/main/kotlin/com/arkon/copyassets/app/MyApp.kt
@@ -3,4 +3,4 @@ package com.arkon.copyassets.app
 import com.arkon.copyassets.view.MainView
 import tornadofx.App
 
-class MyApp: App(MainView::class, Styles::class)
+class CopyAssets: App(MainView::class, Styles::class)
